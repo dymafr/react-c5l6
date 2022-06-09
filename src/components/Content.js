6cfa16decee142920893bbb1c0931066c95bccfe
dymafr <erwan1309@hotmail.com>
2022-06-09 15:47:48 +0200

    1 file modified, 2 added

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Content.module.scss';
+import Recipe from './Recipe';
 
 function Content() {
   return (
@@ -7,16 +8,16 @@ function Content() {
       <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
       <div className={`card p-20 ${styles.contentCard}`}>
         <div className={styles.grid}>
-          <div className={styles.elem}>Element</div>
-          <div className={styles.elem}>Element</div>
-          <div className={styles.elem}>Element</div>
-          <div className={styles.elem}>Element</div>
-          <div className={styles.elem}>Element</div>
-          <div className={styles.elem}>Element</div>
-          <div className={styles.elem}>Element</div>
-          <div className={styles.elem}>Element</div>
-          <div className={styles.elem}>Element</div>
-          <div className={styles.elem}>Element</div>
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
         </div>
       </div>
     </div>
