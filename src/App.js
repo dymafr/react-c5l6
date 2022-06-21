df835d210d43a224006d4505ef6d3b96366b38b7
dymafr <contact@dyma.fr>
2022-06-21 12:33:48 +0200

    Revert "1 file modified, 9 deleted"
    This reverts commit 3fe36ec496bae87319c388811580d239a7e6d0ba.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Content from './components/Content';
+import styles from './App.module.scss';
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <div className={`d-flex flex-column ${styles.appContainer}`}>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
